95051d1355b83fd5a2d7ba0951d10443f091a48d
shuh <nonya@biz.com>
2019-07-27 17:23:17 -0400

    updated server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-let bodyParser = require("body-parser");
-let mongoose = require("mongoose");
-let logger = require("morgan");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 
-let express = require("express");
-let app = express();
+var express = require("express");
+var app = express();
 
 app.use(logger("dev"));
 app.use(
@@ -12,6 +12,13 @@ app.use(
   })
 );
 
-let exphbs = require("express-handlebars");
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
 app.engine("handlebars",exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT. Visit http://localhost:" + port);
+});
