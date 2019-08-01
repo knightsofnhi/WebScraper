ae86df2dc764dd3a04f49375f96b348fb109b799
shuh <nonya@biz.com>
2019-08-01 17:20:35 -0400

    capitalized

--- controller/controller.js
+++ controller/controller.js
@@ -9,7 +9,7 @@ var Comment = require("../models/Comment.js");
 var Article = require("../models/Article.js");
 
 router.get("/", function(req, res) {
-  res.redirect("/articles");
+  res.redirect("/WebScraper/public/index.html/articles");
 });
 
 router.get("/scrape", function(req, res) {
@@ -57,7 +57,7 @@ router.get("/scrape", function(req, res) {
     res.redirect("/");
   });
 });
-router.get("/articles", function(req, res) {
+router.get("/WebScraper/public/index.html/articles", function(req, res) {
   Article.find()
     .sort({ _id: -1 })
     .exec(function(err, doc) {
@@ -70,7 +70,7 @@ router.get("/articles", function(req, res) {
     });
 });
 
-router.get("/articles-json", function(req, res) {
+router.get("/WebScraper/public/index.html/articles-json", function(req, res) {
   Article.find({}, function(err, doc) {
     if (err) {
       console.log(err);
@@ -88,7 +88,7 @@ router.get("/clearAll", function(req, res) {
       console.log("removed all articles");
     }
   });
-  res.redirect("/articles-json");
+  res.redirect("/WebScraper/public/index.html/articles-json");
 });
 
 router.get("/readArticle/:id", function(req, res) {
